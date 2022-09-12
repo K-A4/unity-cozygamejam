848a8d00a0cf143a8b5f1b36279794d9c2c8ce7f
Ifstx <61512571+FstSlacker@users.noreply.github.com>
2022-09-12 20:42:19 +0300

    Items base classes

---
+++ Assets/Scripts/RoomItem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EPlacementRules : int
+{
+    Floor   = 0,
+    Surface = 1,
+    Wall    = 2,
+    Ceil    = 4
+}
+
+[System.Serializable]
+public class RoomItemInfo
+{
+    public float MaxHealth = 0;
+    public float CozyPerBuy = 0;
+    public float CozyPerUse = 0;
+}
+
+public abstract class RoomItem : MonoBehaviour
+{
+    public abstract int PlacementRules { get; }
+    public float Health { get; protected set; } = 0;
+    public RoomItemInfo info { get; protected set; }
+
+    public void SetParams(RoomItemInfo info)
+    {
+        this.info = info;
+        Health = info.MaxHealth;
+    }
+
+    public abstract void Use();
+}
