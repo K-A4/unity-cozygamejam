5469cbbd0c8ff15b72f769bc0d0de9391e45e233
K-A4 <64972979+K-A4@users.noreply.github.com>
2022-09-13 17:17:07 +0500

    added PlayerPrefab and updatedCameraLook

--- Assets/CameraLook.cs
+++ Assets/CameraLook.cs
@@ -6,23 +6,22 @@ public class CameraLook : MonoBehaviour
 {
     [SerializeField] private float sensetivity;
     [SerializeField] private Transform playerTransform;
-    private Vector3 prevMouse;
     private float angle;
-    private Vector3 mousePosition () => new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+    private Vector3 mousePosition() => new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
 
     private void Start()
     {
-        prevMouse = mousePosition();
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
-        var deltamouse = mousePosition() - prevMouse;
+        var deltamouse = mousePosition();
+
         angle -= deltamouse.y * sensetivity;
         angle = Mathf.Clamp(angle, -90, 90);
         transform.localRotation = Quaternion.Euler(angle, 0,0);
-        prevMouse = mousePosition();
+
         playerTransform.rotation = Quaternion.Euler(0, deltamouse.x * sensetivity, 0) * playerTransform.rotation;
     }
 }
